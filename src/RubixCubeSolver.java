553f369ce7a73b6cbeaaed2a1434e056609ab8b9
jie lian <imjeee@gmail.com>
2011-05-09 03:55:24 -0500

    added a little more code, once face turn works

--- src/RubixCubeSolver.java
+++ src/RubixCubeSolver.java
@@ -3,6 +3,7 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.LineNumberReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 import java.util.regex.Pattern;
 
@@ -11,15 +12,27 @@ public class RubixCubeSolver {
 	static String cF = ".*:.*";
 	static Pattern conf = Pattern.compile(cF, Pattern.CASE_INSENSITIVE);
 
-	static char[][] front = new char[3][3];
-	static char[][] back = new char[3][3];
-	static char[][] top = new char[3][3];
-	static char[][] bottom = new char[3][3];
-	static char[][] left = new char[3][3];
-	static char[][] right = new char[3][3];
+	static char[][] yellow = new char[3][3];
+	static char[][] white = new char[3][3];
+	static char[][] blue = new char[3][3];
+	static char[][] green = new char[3][3];
+	static char[][] red = new char[3][3];
+	static char[][] orange = new char[3][3];
+	
+	static ArrayList<char[][]> yellowNei = new ArrayList<char[][]>();
+	static ArrayList<char[][]> blueNei = new ArrayList<char[][]>();
+	static ArrayList<char[][]> orangeNei = new ArrayList<char[][]>();
+	static ArrayList<char[][]> redNei = new ArrayList<char[][]>();
+	static ArrayList<char[][]> whiteNei = new ArrayList<char[][]>();
+	static ArrayList<char[][]> GreenNei = new ArrayList<char[][]>();
+
+
+
+
 
 	public static void main(String[] args) throws Exception {
 
+		
 		if (args.length != 1) {
 			System.out.println("usage: RubixCubeSolver <configuration file>");
 		}
@@ -43,48 +56,154 @@ public class RubixCubeSolver {
 				build(line);
 			}
 		}
+		
+		constructFaceNeighbors();
 
-		/*
-		 * top:ggggggggg 
-		 * bottom:bbbbbbbbb 
-		 * front:rrrrrrrrr 
-		 * back:ooooooooo
-		 * left:yyyyyyyyy 
-		 * right:wwwwwwwww
-		 */
-		printTop();
-		printBottom();
-		printLeft();
-		printRight();
-		printFront();
-		printBack();
+		 
+		printEverything();
+		turnYellow(false);
+		printEverything();
 
 	}
 
+	private static void constructFaceNeighbors() {
 
+		yellowNei.add(blue);
+		yellowNei.add(orange);
+		yellowNei.add(green);
+		yellowNei.add(red);
+		
+		blueNei.add(white);
+		blueNei.add(orange);
+		blueNei.add(yellow);
+		blueNei.add(red);
+		
+		orangeNei.add(blue);
+		orangeNei.add(white);
+		orangeNei.add(green);
+		orangeNei.add(yellow);
+
+		redNei.add(blue);
+		redNei.add(yellow);
+		redNei.add(green);
+		redNei.add(white);
+
+		whiteNei.add(green);
+		whiteNei.add(orange);
+		whiteNei.add(blue);
+		whiteNei.add(red);
+		
+		GreenNei.add(yellow);
+		GreenNei.add(orange);
+		GreenNei.add(white);
+		GreenNei.add(red);	
+	}
+
+	private static void turnYellow(boolean clockWise) {
+
+
+		char[] copyBlueBottom = deepCopy(blue, 2, true);
+		char[] copyGreenTop = deepCopy(green, 0, true);
+		char[] copyOrangeLeft = deepCopy(orange, 0, false);
+		char[] copyRedRight = deepCopy(red, 2, false);
+
+		if(clockWise){
+			turnFaceClock(yellow);
+			replaceColor(blue, copyRedRight, 2, true);
+			replaceColor(orange, copyBlueBottom, 0, false);
+			replaceColor(green, copyOrangeLeft, 0, true);
+			replaceColor(red, copyGreenTop, 2, false);
+		} else {
+			turnFaceCounterClock(yellow);
+			replaceColor(blue, copyOrangeLeft, 2, true);
+			replaceColor(orange, copyGreenTop, 0, false);
+			replaceColor(green, copyRedRight, 0, true);
+			replaceColor(red, copyBlueBottom, 2, false);
+		}
+	
+	}
+
+	private static void turnFaceCounterClock(char[][] yellow2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void replaceColor(char[][] target, char[] source, int j,
+			boolean replaceRow) {
+
+		for (int i = 0; i < 3; i++) {
+			if (replaceRow) {
+				target[j][i] = source[i];
+			} else {
+				target[i][j] = source[i];
+			}
+		}
+	}
+
+	private static char[] deepCopy(char[][] face, int cr, boolean copyRow) {
+
+		char[] result = new char[3];
+
+		for (int i = 0; i < 3; i++) {
+			if (copyRow) {
+				result[i] = face[cr][i];
+			} else {
+				result[i] = face[i][cr];
+			}
+		}
+
+		return result;
+	}
+
+	private static void turnFaceClock(char[][] face) {
+
+		char[][] tempFace = deepCopyFace(face);
+
+		face[0][0] = tempFace[2][0];
+		face[0][1] = tempFace[1][0];
+		face[0][2] = tempFace[0][0];
+
+		face[1][0] = tempFace[2][1];
+		face[1][2] = tempFace[0][1];
+
+		face[2][0] = tempFace[0][2];
+		face[2][1] = tempFace[1][2];
+		face[2][2] = tempFace[2][2];
+	}
+
+	private static char[][] deepCopyFace(char[][] face) {
+
+		char[][] copy = new char[3][3];
+		for (int r = 0; r < 3; r++) {
+			for (int c = 0; c < 3; c++) {
+				copy[r][c] = face[r][c];
+			}
+		}
+		return copy;
+	}
 
 	// parse config file with StringTokenizer
 	private static void build(String line) {
 
 		StringTokenizer st = new StringTokenizer(line, ":");
 
-		String temp = st.nextToken();
-		String color = st.nextToken();
+		String temp = st.nextToken().trim();
+		String colors = st.nextToken().trim();
 
-		System.out.println(temp + ": " + color);
+		System.out.println(temp + ": " + colors);
 
-		if (temp.equals("front")) {
-			buildFace(front, color);
-		} else if (temp.equals("back")) {
-			buildFace(back, color);
-		} else if (temp.equals("top")) {
-			buildFace(top, color);
-		} else if (temp.equals("bottom")) {
-			buildFace(bottom, color);
-		} else if (temp.equals("left")) {
-			buildFace(left, color);
-		} else if (temp.equals("right")) {
-			buildFace(right, color);
+		if (temp.equals("yellow")) {
+			buildFace(yellow, colors);
+		} else if (temp.equals("white")) {
+			buildFace(white, colors);
+		} else if (temp.equals("blue")) {
+			buildFace(blue, colors);
+		} else if (temp.equals("green")) {
+			buildFace(green, colors);
+		} else if (temp.equals("red")) {
+			buildFace(red, colors);
+		} else if (temp.equals("orange")) {
+			buildFace(orange, colors);
 		} else {
 			System.out.println("what side again? " + temp);
 		}
@@ -92,48 +211,59 @@ public class RubixCubeSolver {
 	}
 
 	// build each face.
-	private static void buildFace(char[][] face, String color) {
+	private static void buildFace(char[][] face, String colors) {
 
 		int ch = 0;
 
 		for (int r = 0; r < 3; r++) {
 			for (int c = 0; c < 3; c++) {
-				face[r][c] = color.charAt(ch);
+				face[r][c] = colors.charAt(ch);
 				ch++;
 			}
 		}
 
 	}
 
-	//print a side
-	private static void printTop() {
-		System.out.println("top: ");
-		print(top);
+	// print a side
+	private static void printBlue() {
+		System.out.println("blue: ");
+		print(blue);
 	}
 
-	private static void printBottom() {
-		System.out.println("bottom: ");
-		print(bottom);
+	private static void printGreen() {
+		System.out.println("green: ");
+		print(green);
 	}
 
-	private static void printLeft() {
-		System.out.println("left: ");
-		print(left);
+	private static void printRed() {
+		System.out.println("red: ");
+		print(red);
 	}
 
-	private static void printRight() {
-		System.out.println("right: ");
-		print(right);
+	private static void printOrange() {
+		System.out.println("orange: ");
+		print(orange);
 	}
 
-	private static void printFront() {
-		System.out.println("front: ");
-		print(front);
+	private static void printYellow() {
+		System.out.println("yellow: ");
+		print(yellow);
 	}
 
-	private static void printBack() {
-		System.out.println("back: ");
-		print(back);
+	private static void printWhite() {
+		System.out.println("white: ");
+		print(white);
+	}
+
+	private static void printEverything() {
+		System.out.println("print EVERYTHING: ");
+		printBlue();
+		printGreen();
+		printRed();
+		printOrange();
+		printYellow();
+		printWhite();
+		System.out.println();
 	}
 
 	private static void print(char[][] face) {
