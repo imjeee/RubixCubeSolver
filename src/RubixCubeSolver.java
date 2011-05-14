178bc909a948c397cdd7258a9b33057551f7a28a
Ryan Stewart <rstewart@cs.utexas.edu>
2011-05-13 21:32:10 -0500

    printcube added, using log instead of printlns

--- src/RubixCubeSolver.java
+++ src/RubixCubeSolver.java
@@ -9,30 +9,31 @@ import java.util.regex.Pattern;
 
 public class RubixCubeSolver {
 
-	static String cF = ".*:.*";
-	static Pattern conf = Pattern.compile(cF, Pattern.CASE_INSENSITIVE);
+	// flags
+	private static boolean stdout = true;
+	
+	
+	private static String cF = ".*:.*";
+	private static Pattern conf = Pattern.compile(cF, Pattern.CASE_INSENSITIVE);
 
-	static char[][] yellow = new char[3][3]; // yellow is front
-	static char[][] white = new char[3][3]; // white is back
-	static char[][] blue = new char[3][3]; // blue is top
-	static char[][] green = new char[3][3]; // green is bottom
-	static char[][] red = new char[3][3]; // red is left
-	static char[][] orange = new char[3][3]; // orange is right
+	private static char[][] yellow = new char[3][3]; // yellow is front
+	private static char[][] white = new char[3][3]; // white is back
+	private static char[][] blue = new char[3][3]; // blue is top
+	private static char[][] green = new char[3][3]; // green is bottom
+	private static char[][] red = new char[3][3]; // red is left
+	private static char[][] orange = new char[3][3]; // orange is right
 
-	static ArrayList<char[][]> yellowNei = new ArrayList<char[][]>();
-	static ArrayList<char[][]> blueNei = new ArrayList<char[][]>();
-	static ArrayList<char[][]> orangeNei = new ArrayList<char[][]>();
-	static ArrayList<char[][]> redNei = new ArrayList<char[][]>();
-	static ArrayList<char[][]> whiteNei = new ArrayList<char[][]>();
-	static ArrayList<char[][]> GreenNei = new ArrayList<char[][]>();
+	
+	
+	
+	
 
 	public static void main(String[] args) throws Exception {
 
 		if (args.length != 1) {
-			System.out.println("usage: RubixCubeSolver <configuration file>");
+			logln("usage: RubixCubeSolver <configuration file>");
 		}
 
-		//
 		File configFile = null;
 		FileReader fr = null;
 
@@ -40,7 +41,7 @@ public class RubixCubeSolver {
 			configFile = new File(args[0]);
 			fr = new FileReader(configFile);
 		} catch (IOException e) {
-			System.out.println("cannot read source file: " + args[0]);
+			logln("cannot read source file: " + args[0]);
 		}
 
 		String line;
@@ -52,12 +53,11 @@ public class RubixCubeSolver {
 			}
 		}
 
-		// printEverything();
-		// turnYellow(true);
-		// turnBlue(true);
-		// turnOrange(true);
-		printEverything();
 
+		printCube();
+		logln("turning yellow clockwise");
+		turnYellow(true);
+		printCube();
 	}
 
 	private static void turnYellow(boolean clockWise) {
@@ -74,7 +74,7 @@ public class RubixCubeSolver {
 			replaceColor(orangec, green, "top");
 			replaceColor(greenc, red, "right");
 
-			System.out.println("turn yellow clockwise");
+			logln("turn yellow clockwise");
 
 		} else {
 			turnFaceCounterClock(yellow);
@@ -82,7 +82,7 @@ public class RubixCubeSolver {
 			replaceColor(greenc, orange, "left");
 			replaceColor(redc, green, "top");
 			replaceColor(bluec, red, "right");
-			System.out.println("turn yellow counter clockwise \n");
+			logln("turn yellow counter clockwise \n");
 
 		}
 
@@ -102,7 +102,7 @@ public class RubixCubeSolver {
 			replaceColor(orangec, yellow, "top");
 			replaceColor(yellowc, red, "top");
 
-			System.out.println("turn blue clockwise");
+			logln("turn blue clockwise");
 
 		} else {
 			turnFaceCounterClock(blue);
@@ -111,7 +111,7 @@ public class RubixCubeSolver {
 			replaceColor(redc, yellow, "top");
 			replaceColor(whitec, red, "top");
 
-			System.out.println("turn blue counter clockwise \n");
+			logln("turn blue counter clockwise \n");
 		}
 
 	}
@@ -130,7 +130,7 @@ public class RubixCubeSolver {
 			replaceColor(whitec, green, "right");
 			replaceColor(greenc, yellow, "right");
 
-			System.out.println("turn orange clockwise \n");
+			logln("turn orange clockwise \n");
 
 		} else {
 			turnFaceCounterClock(orange);
@@ -139,7 +139,7 @@ public class RubixCubeSolver {
 			replaceColor(yellowc, green, "right");
 			replaceColor(bluec, yellow, "right");
 
-			System.out.println("turn orange counter clockwise \n");
+			logln("turn orange counter clockwise \n");
 		}
 
 	}
@@ -158,7 +158,7 @@ public class RubixCubeSolver {
 			replaceColor(redc, green, "bottom");
 			replaceColor(greenc, orange, "right");
 
-			System.out.println("turn white clockwise \n");
+			logln("turn white clockwise \n");
 
 		} else {
 			turnFaceCounterClock(white);
@@ -167,7 +167,7 @@ public class RubixCubeSolver {
 			replaceColor(orangec, green, "bottom");
 			replaceColor(bluec, orange, "right");
 
-			System.out.println("turn white counter clockwise \n");
+			logln("turn white counter clockwise \n");
 		}
 
 	}
@@ -186,7 +186,7 @@ public class RubixCubeSolver {
 			replaceColor(orangec, white, "bottom");
 			replaceColor(whitec, red, "bottom");
 
-			System.out.println("turn green clockwise \n");
+			logln("turn green clockwise \n");
 
 		} else {
 			turnFaceCounterClock(green);
@@ -195,7 +195,7 @@ public class RubixCubeSolver {
 			replaceColor(redc, white, "bottom");
 			replaceColor(yellowc, red, "bottom");
 
-			System.out.println("turn green counter clockwise \n");
+			logln("turn green counter clockwise \n");
 		}
 
 	}
@@ -214,7 +214,7 @@ public class RubixCubeSolver {
 			replaceColor(yellowc, green, "left");
 			replaceColor(greenc, white, "right");
 
-			System.out.println("turn green clockwise \n");
+			logln("turn green clockwise \n");
 
 		} else {
 			turnFaceCounterClock(green);
@@ -223,7 +223,7 @@ public class RubixCubeSolver {
 			replaceColor(greenc, yellow, "left");
 			replaceColor(whitec, green, "left");
 
-			System.out.println("turn green counter clockwise \n");
+			logln("turn green counter clockwise \n");
 		}
 
 	}
@@ -273,7 +273,7 @@ public class RubixCubeSolver {
 			} else if (side.equals("right")) {
 				target[i][2] = source[i];
 			} else {
-				System.out.println("error determining side (replaceColor)");
+				logln("error determining side (replaceColor)");
 			}
 		}
 	}
@@ -292,7 +292,7 @@ public class RubixCubeSolver {
 			} else if (side.equals("right")) {
 				result[i] = face[i][2];
 			} else {
-				System.out.println("error determining side (deepcopy)");
+				logln("error determining side (deepcopy)");
 			}
 		}
 
@@ -311,36 +311,45 @@ public class RubixCubeSolver {
 	}
 
 	// parse config file with StringTokenizer
-	private static void build(String line) {
+	private static boolean build(String line) {
 
 		StringTokenizer st = new StringTokenizer(line, ":");
 
 		String temp = st.nextToken().trim();
 		String colors = st.nextToken().trim();
 
-		System.out.println(temp + ": " + colors);
+		logln(temp + ": " + colors);
 
+		boolean result = true;
+		
 		if (temp.equals("yellow")) {
-			buildFace(yellow, colors);
+			result = buildFace(yellow, colors, temp);
 		} else if (temp.equals("white")) {
-			buildFace(white, colors);
+			result = buildFace(white, colors, temp);
 		} else if (temp.equals("blue")) {
-			buildFace(blue, colors);
+			result = buildFace(blue, colors, temp);
 		} else if (temp.equals("green")) {
-			buildFace(green, colors);
+			result = buildFace(green, colors, temp);
 		} else if (temp.equals("red")) {
-			buildFace(red, colors);
+			result = buildFace(red, colors, temp);
 		} else if (temp.equals("orange")) {
-			buildFace(orange, colors);
+			result = buildFace(orange, colors, temp);
 		} else {
-			System.out.println("what side again? " + temp);
+			logln("There is no \"" + temp + "\" side");
+			result = false;
 		}
-
+		return result;
 	}
 
 	// build each face.
-	private static void buildFace(char[][] face, String colors) {
+	private static boolean buildFace(char[][] face, String colors, String color) {
 
+		if (face[0][0] != 0){
+			logln("Already built \"" + color + "\" face");
+			return false;
+		}
+		
+		
 		int ch = 0;
 
 		for (int r = 0; r < 3; r++) {
@@ -350,58 +359,134 @@ public class RubixCubeSolver {
 			}
 		}
 
+		return true;
 	}
 
 	// print a side
 	private static void printBlue() {
-		System.out.println("blue: ");
+		logln("blue: ");
 		print(blue);
 	}
 
 	private static void printGreen() {
-		System.out.println("green: ");
+		logln("green: ");
 		print(green);
 	}
 
 	private static void printRed() {
-		System.out.println("red: ");
+		logln("red: ");
 		print(red);
 	}
 
 	private static void printOrange() {
-		System.out.println("orange: ");
+		logln("orange: ");
 		print(orange);
 	}
 
 	private static void printYellow() {
-		System.out.println("yellow: ");
+		logln("yellow: ");
 		print(yellow);
 	}
 
 	private static void printWhite() {
-		System.out.println("white: ");
+		logln("white: ");
 		print(white);
 	}
 
 	private static void printEverything() {
-		System.out.println("print EVERYTHING: ");
+		logln("print EVERYTHING: ");
 		printBlue();
 		printGreen();
 		printRed();
 		printOrange();
 		printYellow();
 		printWhite();
-		System.out.println();
+		logln();
 	}
 
+	private static void printCube(){
+		log(cubeToString());
+	}
+
+	private static String cubeToString() {
+		StringBuffer result = new StringBuffer();
+
+		String blank = "      \n      \n      \n";
+		result.append(interleave(blank, faceToString(blue)));
+		result.append(interleave(interleave(faceToString(red), faceToString(yellow)), faceToString(orange)));
+		result.append(interleave(blank, faceToString(green)));
+		result.append(interleave(blank, faceToString(white, true)));
+		return result.toString();
+		
+	}
+
+	private static String faceToString(char[][] face){
+		StringBuffer result = new StringBuffer();
+		for (int r = 0; r < 3; r++) {
+			for (int c = 0; c < 3; c++) {
+				result.append(face[r][c] + " ");
+			}
+			result.append("\n");
+
+		}
+		return result.toString();
+	}
+	
+	private static String faceToString(char[][] face, boolean upsideDown){
+		if (upsideDown = false)
+			return faceToString(face);
+		char[][] newFace = deepCopyFace(face);
+		char temp = newFace[0][1];
+		newFace[0][1] = newFace[0][2];
+		newFace[0][2] = temp;
+		
+		for(int i = 0; i < 3; i++){
+			temp = newFace[0][i];
+			newFace[0][i] = newFace[2][2-i];
+			newFace[2][2-i] = temp;
+		}
+		
+		return faceToString(newFace);
+		
+	}
+	
+	private static String interleave(String x, String y){
+		String[] xtokens = x.split("\n");
+		String[] ytokens = y.split("\n");
+		
+		int max = xtokens.length > ytokens.length ? xtokens.length : ytokens.length;
+		
+		StringBuffer result = new StringBuffer();
+		
+		for(int i = 0; i < max; i++){
+			result.append(xtokens[i] + ytokens[i] + "\n");
+		}
+		
+		return result.toString();
+	}
+	
 	private static void print(char[][] face) {
 		for (int r = 0; r < 3; r++) {
 			for (int c = 0; c < 3; c++) {
-				System.out.print(" " + face[r][c]);
+				log(" " + face[r][c]);
 			}
-			System.out.println();
+			logln();
 		}
 
 	}
+	
+	private static void logln(){
+		log("\n");
+	}
+	
+	private static void logln(String s){
+		log(s + "\n");
+	}
+	
+	private static void log(String s){
+		if (stdout)
+			System.out.print(s);
+	}
+	
 
 }
